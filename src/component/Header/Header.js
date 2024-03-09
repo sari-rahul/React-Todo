02bb61379a94f6b54f56aab14c17a31dd5583c60
sarirahul <sarisivadas93@gmail.com>
2024-03-09 12:49:17 +0100

    Edit paths

--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -6,7 +6,7 @@ const Header = ()=>{
 
     return (
         <header>
-            <Link to="/">Home</Link>
+            <Link to="">Home</Link>
             <Link to="/about">About</Link>
         </header>
     );
